f6ea9789ac2b5db653d92a499e57f17ab8ffbdaa
ArmandoSandino <jsandino@arkkosoft.net>
2023-03-03 14:36:18 -0600

    chore; add product resource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name'=> $this->name,
+            'detail' => $this->detail,
+            'created_at'=> $this->created_at->format('d/m/y'),
+            'updated_at'=> $this->updated_at->format('d/m/y')
+        ];
+    }
+}
